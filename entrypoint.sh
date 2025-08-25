3f42cbf1359a4256091347e670064f998ecaef14
ESolucoes <enzo.trabalho.cel@gmail.com>
2025-08-25 05:07:03 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
-
 echo "🚀 Rodando migrations..."
-node node_modules/typeorm/cli.js -d dist/data-source.js migration:run
-
+node node_modules/typeorm/cli.js -d dist/src/data-source.js migration:run
 echo "✅ Migrations concluídas, iniciando aplicação..."
 exec node dist/main.js
