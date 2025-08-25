b4d35d9cf44c6329ed11151e1230456d99c57e4f
ESolucoes <enzo.trabalho.cel@gmail.com>
2025-08-25 05:04:50 -0300

    feat(api): <descrição do que foi feito>

---
+++ src/data-source.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+
+import { Usuario } from './usuarios/usuario.entity'
+import { CodigoRedefinicao } from './autenticacao/codigo-redefinicao.entity'
+
+import { CriarUsuarios1710000000000 } from './migrations/1710000000000-CriarUsuarios'
+import { CriarCodigosRedefinicaoSenha1710000000001 } from './migrations/1710000000001-CriarCodigosRedefinicaoSenha'
+import { CriarVigencias1710000000002 } from './migrations/1710000000002-CriarVigencias'
+import { CriarMentores1710000000003 } from './migrations/1710000000003-CriarMentores'
+import { CriarMentorados1710000000004 } from './migrations/1710000000004-CriarMentorados'
+import { CriarChatTabelas1710000000005 } from './migrations/1710000000005-CriarTabelaChats'
+import { AdicionarAvatarUsuarios1710000000006 } from './migrations/1710000000006-AdicionarAvatarUsuarios'
+import { AdicionarCurriculoMentorados1710000000007 } from './migrations/1710000000007-AdicionarCurriculoMentorados'
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [Usuario, CodigoRedefinicao],
+  migrations: [
+    CriarUsuarios1710000000000,
+    CriarCodigosRedefinicaoSenha1710000000001,
+    CriarVigencias1710000000002,
+    CriarMentores1710000000003,
+    CriarMentorados1710000000004,
+    CriarChatTabelas1710000000005,
+    AdicionarAvatarUsuarios1710000000006,
+    AdicionarCurriculoMentorados1710000000007,
+  ],
+  synchronize: false,
+  logging: false,
+})
