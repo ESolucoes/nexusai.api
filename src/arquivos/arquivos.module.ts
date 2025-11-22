2b885f8a818698b110c4d88c850de16e5fe44166
ESolucoes <enzo.trabalho.cel@gmail.com>
2025-11-22 01:10:53 -0300

    e la vamos nos

--- src/arquivos/arquivos.module.ts
+++ src/arquivos/arquivos.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ArquivosService } from './arquivos.service'
 import { UploadsController } from './uploads.controller'
-import { ServeStaticModule } from '@nestjs/serve-static'
-import { join } from 'path'
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'uploads', 'public'),
-      serveRoot: '/uploads',
-    }),
-  ],
+  imports: [],
   providers: [ArquivosService],
   controllers: [UploadsController],
   exports: [ArquivosService],
